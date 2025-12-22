cd64b1ccf0ec0c9a3aa7accd384282a28e3c27ba
6704062610178 <s6704062610178@email.kmutnb.ac.th>
2025-12-22 11:08:47 +0700

    Add files via upload

---
+++ lab4no5.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	int i,h,s;
+	scanf("%d %d",&h,&s);
+	for(i=0;i<s;i++){
+		printf("%d",h);
+	}
+}
---
+++ lab4no6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int i,j,h,s;
+	scanf("%d %d",&h,&s);
+	for(i=0;i<s;i++){
+		for(j=0;j<s;j++){
+			printf("%d",h);
+		}
+		printf("\n");
+	}
+}
