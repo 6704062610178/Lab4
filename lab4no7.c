0954136dcf476c05da29d3f6ab39eb28237ba4c4
6704062610178 <s6704062610178@email.kmutnb.ac.th>
2025-12-22 11:26:06 +0700

    Add files via upload

---
+++ lab4no7.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(){
+	int score,a,o,i;
+	scanf("%d",&o);
+	for( i = 0 ; i<o;i++){
+		scanf("%d",&score);
+		if(score > 100){
+			printf("error score\n");
+		}
+		else if( score < 0){
+			printf("error score\n");
+		}
+		else if(score >= 68){
+			if(score >=85){
+				printf("%d(A)\n",score);
+			}
+			else if(score >=75){
+				printf("%d(B)\n",score);
+			}
+			else if(score>=68){
+				printf("%d(C)\n",score);
+			}
+		}
+		else{
+			if(score >=55){
+				printf("&d(D)\n",score);
+			}
+			else if(score<55){
+				printf("%d(F)\n",score);
+			}
+		}	
+	}
+	
+}
